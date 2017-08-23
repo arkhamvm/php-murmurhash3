47b77f89397b59b0c0418bc04296d88b5c88b0e7
Vladimir <arkhamvm@users.noreply.github.com>
2017-08-23 16:33:57 +1000

    Delete install.sh

--- build/install.sh
+++
@@ -1,5 +0,0 @@
-git clone git://github.com/j42/MurmurHash3PHP.git ~/tmp/murmurhash
-cd ~/tmp/murmurhash && phpize && ./configure --enable-murmurhash3 && make install
-[ -s /etc/php5/fpm/conf.d/20-murmurhash3.ini ] && echo "murmurhash3.ini already exists" || echo extension=murmurhash3.so | tee -a /etc/php5/fpm/conf.d/20-murmurhash3.ini
-[ -s /etc/php5/cli/conf.d/20-murmurhash3.ini ] && echo "murmurhash3.ini already exists" || echo extension=murmurhash3.so | tee -a /etc/php5/cli/conf.d/20-murmurhash3.ini
-rm -rf ~/tmp/murmurhash
